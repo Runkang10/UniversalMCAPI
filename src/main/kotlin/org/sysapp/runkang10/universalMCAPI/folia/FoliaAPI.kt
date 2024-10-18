5f766e43441de5b884d6dfc33a8677da4a4d5ad9
Runkang Chen <chen.runkang10@gmail.com>
2024-10-18 18:36:02 +0200

    feat(SendConsoleMessage)

---
+++ src/main/kotlin/org/sysapp/runkang10/universalMCAPI/folia/FoliaAPI.kt
@@ -0,0 +1,52 @@
+package org.sysapp.runkang10.universalMCAPI.folia
+
+import io.papermc.paper.threadedregions.scheduler.GlobalRegionScheduler
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+import org.sysapp.runkang10.universalMCAPI.UniversalMCAPI
+import org.sysapp.runkang10.universalMCAPI.paper.console.SendMessage
+import kotlin.math.log
+
+class FoliaAPI(private var plugin: JavaPlugin) {
+    private var isfolia: Boolean = this.isFolia()
+    private var logger: SendMessage = UniversalMCAPI().getServerLogger()
+    private lateinit var globalRegionScheduler: GlobalRegionScheduler
+
+    fun init() {
+        this.run {
+            globalRegionScheduler = plugin.server.globalRegionScheduler
+            logger.info("Folia configuration has been set up successfully.")
+        }
+    }
+
+    private fun isFolia(): Boolean {
+        return try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer")
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun runFoliaCode(action: () -> Unit, async: Boolean = false): Boolean {
+        return if (isfolia) {
+            if (async) {
+                Bukkit.getScheduler().runTaskAsynchronously(plugin, Runnable {
+                    action.invoke()
+                })
+            } else {
+                action.invoke()
+            }
+            true
+        } else {
+            logger.warning("=================================================")
+            logger.warning("  Cannot use `$action` on this server.")
+            logger.warning("")
+            logger.warning("  Please check if your server uses Folia.")
+            logger.warning("=================================================")
+            false
+        }
+    }
+}
